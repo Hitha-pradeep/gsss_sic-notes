3759b6f0cbd7c23be96cef4b3d53d4bffdbe0918
Hitha-pradeep <hithapradeep15@gmail.com>
2025-08-19 09:48:31 +0530

    Delete day_3 directory

--- day_3/math_tabels.py
+++
@@ -1,6 +0,0 @@
-import sys 
-number=int(sys.argv[1])
-print(f"user given a number is {number}")
-for i in range (1,21):
-    print(f"%d*%2d=%3d (number * i = number * i)")  
-    #print(f"{number} * {i} = {number * i}") this  will get op but not formated  alignment
